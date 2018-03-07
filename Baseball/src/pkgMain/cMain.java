a2fe5dbb2f8304a68f7f5e010b11bbb5fa36007f
Bert.Gibbons <Bert.Gibbons@APHCNB101375.amed.ds.army.mil>
2018-03-06 20:21:23 -0500

    change comment

--- Baseball/src/pkgMain/cMain.java
+++ Baseball/src/pkgMain/cMain.java
@@ -3,7 +3,7 @@ package pkgMain;
 public class cMain {
 
 	public static void main(String[] args) {
-		
+		// my change
 		Rectangle rec1 = new Rectangle(10,20);
 		
 		System.out.println(rec1.area());
